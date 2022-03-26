b8b459957acce546fb78acc6fc83f84561f30da5
kaznak <kaznak.at.work@gmail.com>
2022-03-27 03:48:12 +0900

    wip

--- src/backend/file.rs
+++ src/backend/file.rs
@@ -2,34 +2,44 @@ use serde::{Deserialize, Serialize};
 use std::{fs::File, io::Read};
 
 #[derive(Deserialize, Serialize, PartialEq, Debug)]
-pub struct PostMetaData {
+pub struct PostFrontMatter {
     pub title: String,
 }
 
+#[derive(PartialEq, Debug)]
+pub struct PostMetaData {
+    pub front_matter: PostFrontMatter,
+    pub slug: String,
+}
+
 pub fn list_posts(posts_dir: &str) -> Vec<PostMetaData> {
-    let mut metadata: Vec<PostMetaData> = Vec::new();
+    let mut postdata: Vec<PostMetaData> = Vec::new();
     match std::fs::read_dir(posts_dir) {
         Err(e) => {
             log::warn!("{:?}", e);
         }
         Ok(paths) => {
-            log::trace!("{:?}", paths);
-            for path in paths {
-                let mut file = File::open(path.unwrap().path()).unwrap();
-                log::trace!("{:?}", file);
+            // log::trace!("{:?}", paths);
+            for direntry_result in paths {
+                let path = direntry_result.unwrap().path();
+                let slug = path.file_stem().unwrap().to_str().unwrap().to_string();
+                let mut file = File::open(path).unwrap();
+                // log::trace!("{:?}", file);
                 let mut cont = String::new();
-                log::trace!("{:?}", cont);
-                let n = file.read_to_string(&mut cont).unwrap();
-                log::trace!("{:?}", n);
-                let (front_matter, content) =
-                    serde_frontmatter::deserialize::<PostMetaData>(&cont).unwrap();
-                log::trace!("{:?}", front_matter);
-                log::trace!("{:?}", content);
-                metadata.push(front_matter);
+                // log::trace!("{:?}", cont);
+                let _n = file.read_to_string(&mut cont).unwrap();
+                // log::trace!("{:?}", _n);
+                let (front_matter, _content) =
+                    serde_frontmatter::deserialize::<PostFrontMatter>(&cont).unwrap();
+                // log::trace!("{:?}", front_matter);
+                // log::trace!("{:?}", _content);
+                let metadata = PostMetaData { front_matter, slug };
+                postdata.push(metadata);
             }
         }
     };
-    metadata
+    log::trace!("{:?}", postdata);
+    postdata
 }
 
 #[cfg(test)]
@@ -39,8 +49,10 @@ mod tests {
     fn success() {
         let _ = pretty_env_logger::try_init();
         let metadata = list_posts("./example/posts");
-        assert!(metadata[0].title.eq("sample 1"));
-        assert!(metadata[1].title.eq("sample 2"));
+        assert!(metadata[0].slug.eq("sample1"));
+        assert!(metadata[0].front_matter.title.eq("sample 1"));
+        assert!(metadata[1].slug.eq("sample2"));
+        assert!(metadata[1].front_matter.title.eq("sample 2"));
     }
     #[test]
     fn not_exists() {
