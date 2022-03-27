b79a3cff56bdf67e429720bdd557e0d41cd9d0d8
Nakamura Kazutaka <kaznak.at.work@gmail.com>
2022-03-28 00:54:18 +0900

    setup
    axum, async-graphql をセットアップ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::{EmptySubscription, Schema};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::extract::Extension;
+use axum::response::{Html, IntoResponse};
+use axum::routing::get;
+use axum::Router;
+use hyper::{Method, Server};
+use tower_http::cors::{CorsLayer, Origin};
+
+use rust_axum_async_graphql_blog::graphql::model::{FilesSchema, MutationRoot, QueryRoot, Storage};
+
+async fn graphql_handler(schema: Extension<FilesSchema>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.0).await.into()
+}
+
+async fn graphql_playground() -> impl IntoResponse {
+    Html(playground_source(GraphQLPlaygroundConfig::new("/")))
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
+        .data(Storage::default())
+        .finish();
+
+    println!("Playground: http://localhost:8000");
+
+    let app = Router::new()
+        .route("/", get(graphql_playground).post(graphql_handler))
+        .layer(Extension(schema))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Origin::predicate(|_, _| true))
+                .allow_methods(vec![Method::GET, Method::POST]),
+        );
+
+    Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
