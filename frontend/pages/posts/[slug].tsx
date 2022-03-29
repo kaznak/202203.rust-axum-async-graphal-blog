365bad1f46638d7dadaa86adc5b2bbf5a4a94a67
kaznak <kaznak.at.work@gmail.com>
2022-03-29 23:47:48 +0900

    fix

--- frontend/pages/posts/[slug].tsx
+++ frontend/pages/posts/[slug].tsx
@@ -8,11 +8,14 @@ import vercelLogoSvg from 'public/vercel.svg'
 export function Post() {
   const router = useRouter()
   const { slug } = router.query
-  console.log(slug)
-  const { data, error } =
-    typeof slug == 'string' ? useBlogPost(slug) : useBlogPost()
-  console.log({ data, error })
+  const { data, error } = useBlogPost(slug as string)
 
+  if (error) {
+    return <div>failed to load</div>
+  }
+  if (!data) {
+    return <div>loading...</div>
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -20,16 +23,10 @@ export function Post() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {error ? (
-        <div>failed to load</div>
-      ) : !data ? (
-        <div>loading...</div>
-      ) : (
-        <main className="w-full flex-1 px-20">
-          <h1 className="text-6xl font-bold border-b-8">{data.post.title}</h1>
-          <section>{data.post.content}</section>
-        </main>
-      )}
+      <main className="w-full flex-1 px-20">
+        <h1 className="text-6xl font-bold border-b-8">{data.post.title}</h1>
+        <section>{data.post.content}</section>
+      </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
