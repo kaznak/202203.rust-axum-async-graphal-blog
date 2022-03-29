2b4504d46ef4bee99877328fc8b5a6b34640baab
kaznak <kaznak.at.work@gmail.com>
2022-03-29 12:32:16 +0900

    update

--- backend/src/graphql/model.rs
+++ backend/src/graphql/model.rs
@@ -1,9 +1,10 @@
 use crate::backend::{file::FileBackend, post::*};
-use async_graphql::{Context, EmptySubscription, Object, Schema, SimpleObject, ID};
+use async_graphql::{Context, EmptySubscription, InputObject, Object, Schema, SimpleObject, ID};
 
 pub type GraphQLSchema = Schema<QueryRoot, MutationRoot, EmptySubscription>;
 
-#[derive(Clone, SimpleObject)]
+#[derive(Clone, SimpleObject, InputObject)]
+#[graphql(input_name = "PostInput")]
 pub struct Post {
     slug: ID,
     title: String,
@@ -55,6 +56,38 @@ impl From<&PostData> for Post {
     }
 }
 
+#[derive(Clone, InputObject)]
+pub struct PostOpt {
+    slug: ID,
+    title: Option<String>,
+    content: Option<String>,
+}
+
+impl PostOpt {
+    fn to_postdata(&self, backend: &dyn Backend) -> Result<PostData, ()> {
+        let res = backend.read_post(&self.slug.to_string());
+        if let Err(_) = res {
+            return Err(());
+        }
+        let PostData {
+            slug,
+            mut title,
+            mut content,
+        } = res.unwrap();
+        if let Some(v) = &self.title {
+            title = v.to_string();
+        }
+        if let Some(v) = &self.content {
+            content = v.to_string();
+        }
+        Ok(PostData {
+            slug,
+            title,
+            content,
+        })
+    }
+}
+
 pub type Storage = FileBackend;
 
 pub struct QueryRoot;
@@ -81,38 +114,22 @@ pub struct MutationRoot;
 
 #[Object]
 impl MutationRoot {
-    async fn create<'ctx>(
-        &self,
-        ctx: &Context<'ctx>,
-        slug: ID,
-        title: String,
-        content: String,
-    ) -> Result<Post, String> {
+    async fn create<'ctx>(&self, ctx: &Context<'ctx>, post: Post) -> Result<Post, String> {
         let backend = ctx.data::<Storage>().unwrap();
-        let postdata = PostData {
-            slug: String::from(slug),
-            title,
-            content,
-        };
+        let postdata = post.into();
         match backend.create_post(&postdata) {
             Ok(p) => Ok(Post::from(p)),
             Err(_) => Err("error!".to_string()),
         }
     }
-    async fn update<'ctx>(
-        &self,
-        ctx: &Context<'ctx>,
-        slug: ID,
-        title: String,
-        content: String,
-    ) -> Result<Post, String> {
+    async fn update<'ctx>(&self, ctx: &Context<'ctx>, postopt: PostOpt) -> Result<Post, String> {
         let backend = ctx.data::<Storage>().unwrap();
-        let postdata = PostData {
-            slug: String::from(slug),
-            title,
-            content,
-        };
-        match backend.create_post(&postdata) {
+        let res_read = postopt.to_postdata(backend);
+        if let Err(_) = res_read {
+            return Err("error!".to_string());
+        }
+        let res_create = backend.create_post(&res_read.unwrap());
+        match res_create {
             Ok(p) => Ok(Post::from(p)),
             Err(_) => Err("error!".to_string()),
         }
