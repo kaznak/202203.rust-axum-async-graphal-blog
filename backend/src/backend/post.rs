a0950bf13111f340368422de4e4fb6722e008f74
Nakamura Kazutaka <kaznak.at.work@gmail.com>
2022-04-03 03:31:53 +0900

    Refactor/layers (#19)
    - バックエンドがエラー構造体を返す
    - BFF(GraphQL)で専用のエラー型を利用する
    - 引数、slug のバリデーションを導入
    - list クエリは slug フィールドのみ要求されている場合はポストの内容を読み込まない

--- backend/src/backend/post.rs
+++ backend/src/backend/post.rs
@@ -1,7 +1,9 @@
+pub type Slug = String;
+
 /// Post のデータ
 #[derive(Clone, PartialEq, Debug)]
 pub struct PostData {
-    pub slug: String,
+    pub slug: Slug,
     pub title: String,
     pub content: String,
 }
@@ -9,13 +11,13 @@ pub struct PostData {
 /// Backend の　trait
 pub trait Backend {
     /// Create
-    fn create_post(&self, postdata: &PostData) -> Result<PostData, ()>;
+    fn create_post(&self, postdata: &PostData) -> Result<PostData, Box<dyn std::error::Error>>;
     /// Read
-    fn read_post(&self, slug: &str) -> Result<PostData, ()>;
+    fn read_post(&self, slug: &str) -> Result<PostData, Box<dyn std::error::Error>>;
     /// List
-    fn list_posts(&self) -> Result<Vec<PostData>, ()>;
+    fn list_posts(&self) -> Result<Vec<Slug>, Box<dyn std::error::Error>>;
     /// Update
-    fn update_post(&self, postdata: &PostData) -> Result<PostData, ()>;
+    fn update_post(&self, postdata: &PostData) -> Result<PostData, Box<dyn std::error::Error>>;
     /// Delete
-    fn delete_post(&self, slug: &str) -> Result<(), ()>;
+    fn delete_post(&self, slug: &str) -> Result<(), Box<dyn std::error::Error>>;
 }
--- backend/src/graphql/model.rs
+++ backend/src/graphql/model.rs
@@ -1,12 +1,15 @@
+use std::collections::HashSet;
+
 use crate::backend::{file::FileBackend, post::*};
-use async_graphql::{Context, EmptySubscription, InputObject, Object, Schema, SimpleObject, ID};
+use async_graphql::{Context, EmptySubscription, InputObject, Object, Schema, SimpleObject};
 
 pub type GraphQLSchema = Schema<QueryRoot, MutationRoot, EmptySubscription>;
 
 #[derive(Clone, SimpleObject, InputObject)]
 #[graphql(input_name = "PostInput")]
 pub struct Post {
-    slug: ID,
+    #[graphql(validator(min_length = 4, max_length = 1024, regex = r"^[a-z0-9-_]+$"))]
+    slug: String,
     title: String,
     content: String,
 }
@@ -34,7 +37,7 @@ impl From<PostData> for Post {
             content,
         } = postdata;
         Post {
-            slug: ID::from(slug),
+            slug,
             title,
             content,
         }
@@ -49,31 +52,29 @@ impl From<&PostData> for Post {
             content,
         } = postdata;
         Post {
-            slug: ID::from(slug),
+            slug: slug.clone(),
             title: title.clone(),
             content: content.clone(),
         }
     }
 }
 
-#[derive(Clone, InputObject)]
+#[derive(Clone, SimpleObject, InputObject)]
+#[graphql(input_name = "PostOptInput")]
 pub struct PostOpt {
-    slug: ID,
+    #[graphql(validator(min_length = 4, max_length = 1024, regex = r"^[a-z0-9-_]+$"))]
+    slug: String,
     title: Option<String>,
     content: Option<String>,
 }
 
 impl PostOpt {
-    fn to_postdata(&self, backend: &dyn Backend) -> Result<PostData, ()> {
-        let res = backend.read_post(&self.slug.to_string());
-        if let Err(_) = res {
-            return Err(());
-        }
+    fn to_postdata(&self, backend: &dyn Backend) -> Result<PostData, Box<dyn std::error::Error>> {
         let PostData {
             slug,
             mut title,
             mut content,
-        } = res.unwrap();
+        } = backend.read_post(&self.slug.to_string())?;
         if let Some(v) = &self.title {
             title = v.to_string();
         }
@@ -88,24 +89,82 @@ impl PostOpt {
     }
 }
 
+impl Into<PostOpt> for PostData {
+    fn into(self) -> PostOpt {
+        let PostData {
+            slug,
+            title,
+            content,
+        } = self;
+        PostOpt {
+            slug,
+            title: Some(title),
+            content: Some(content),
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum BlogError {
+    #[error("Post Not Found")]
+    NotFound,
+    #[error("Internal Server Error")]
+    InternalServerError,
+}
+
 pub type Storage = FileBackend;
 
 pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn post<'ctx>(&self, ctx: &Context<'ctx>, slug: String) -> Result<Post, String> {
+    async fn post<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        #[graphql(validator(min_length = 4, max_length = 1024, regex = r"^[a-z0-9-_]+$"))]
+        slug: String,
+    ) -> Result<Post, BlogError> {
         let backend = ctx.data::<Storage>().unwrap();
-        match backend.read_post(&slug) {
+        match backend.read_post(&slug.as_str()) {
             Ok(p) => Ok(Post::from(p)),
-            Err(_) => Err("error!".to_string()),
+            Err(e) => {
+                if let Some(e) = e.downcast_ref::<std::io::Error>() {
+                    if std::io::ErrorKind::NotFound == e.kind() {
+                        return Err(BlogError::NotFound);
+                    }
+                }
+                Err(BlogError::InternalServerError)
+            }
         }
     }
-    async fn list<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Vec<Post>, String> {
+    async fn list<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Vec<PostOpt>, BlogError> {
         let backend = ctx.data::<Storage>().unwrap();
-        match backend.list_posts() {
-            Ok(l) => Ok(l.iter().map(|pd| Post::from(pd)).collect()),
-            Err(_) => Err("error!".to_string()),
+        let slug_list = match backend.list_posts() {
+            Ok(v) => v,
+            Err(_) => return Err(BlogError::InternalServerError),
+        };
+
+        let mut fields = ctx
+            .field()
+            .selection_set()
+            .map(|field| field.name())
+            .collect::<HashSet<_>>();
+
+        fields.remove("slug");
+        if 0 < fields.capacity() {
+            return Ok(slug_list
+                .iter()
+                .map(|slug| PostOpt {
+                    slug: slug.clone(),
+                    title: None,
+                    content: None,
+                })
+                .collect());
+        } else {
+            return Ok(slug_list
+                .iter()
+                .map(|slug| backend.read_post(&slug).unwrap().into())
+                .collect());
         }
     }
 }
@@ -114,32 +173,37 @@ pub struct MutationRoot;
 
 #[Object]
 impl MutationRoot {
-    async fn create<'ctx>(&self, ctx: &Context<'ctx>, post: Post) -> Result<Post, String> {
+    async fn create<'ctx>(&self, ctx: &Context<'ctx>, post: Post) -> Result<Post, BlogError> {
         let backend = ctx.data::<Storage>().unwrap();
         let postdata = post.into();
         match backend.create_post(&postdata) {
             Ok(p) => Ok(Post::from(p)),
-            Err(_) => Err("error!".to_string()),
+            Err(_) => Err(BlogError::InternalServerError),
         }
     }
-    async fn update<'ctx>(&self, ctx: &Context<'ctx>, postopt: PostOpt) -> Result<Post, String> {
+    async fn update<'ctx>(&self, ctx: &Context<'ctx>, postopt: PostOpt) -> Result<Post, BlogError> {
         let backend = ctx.data::<Storage>().unwrap();
         let res_read = postopt.to_postdata(backend);
         if let Err(_) = res_read {
-            return Err("error!".to_string());
+            return Err(BlogError::InternalServerError);
         }
         let res_create = backend.create_post(&res_read.unwrap());
         match res_create {
             Ok(p) => Ok(Post::from(p)),
-            Err(_) => Err("error!".to_string()),
+            Err(_) => Err(BlogError::InternalServerError),
         }
     }
-    async fn delete<'ctx>(&self, ctx: &Context<'ctx>, slug: ID) -> Result<ID, String> {
+    async fn delete<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        #[graphql(validator(min_length = 4, max_length = 1024, regex = r"^[a-z0-9-_]+$"))]
+        slug: String,
+    ) -> Result<String, BlogError> {
         let backend = ctx.data::<Storage>().unwrap();
         let slug_str = String::from(slug.clone());
         match backend.delete_post(&slug_str) {
             Ok(_) => Ok(slug),
-            Err(_) => Err("error!".to_string()),
+            Err(_) => Err(BlogError::InternalServerError),
         }
     }
 }
