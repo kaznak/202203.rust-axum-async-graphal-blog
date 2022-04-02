ad5299e0aa794623098e83cda1f2e25c2257f64e
kaznak <kaznak.at.work@gmail.com>
2022-04-02 14:31:58 +0900

    update

--- frontend/test/pages/index.test.tsx
+++ frontend/test/pages/index.test.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import {
-  render,
-  fireEvent,
-  waitFor,
-} from 'test/testUtils/testingLibraryWrapper'
+import { render } from 'test/testUtils/testingLibraryWrapper'
 import { Home } from 'pages/index'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
-  })
-
-  it('clicking button triggers alert', async () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Button'))
-    await waitFor(() => expect(window.alert).toHaveBeenCalledTimes(1))
-    expect(window.alert).toHaveBeenCalledWith('pushed')
   })
 })
